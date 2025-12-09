abe1f5d409463f014576ad1752d272ebb7ba916f
cahayaindrasmara <indrasmara.cahaya27@gmail.com>
2025-12-09 15:19:39 +0700

    test

--- belajar-nodejs-restful-api/src/application/web.js
+++ belajar-nodejs-restful-api/src/application/web.js
@@ -5,11 +5,14 @@ import {userRouter} from "../route/api.js";
 import cors from "cors"
 
 export const web = express();
-web.use(cors())
-
-// web.options("*", cors());
-
 web.use(express.json());
+web.use(cors({
+  origin: "https://reactjs-contacts-management.vercel.app",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}))
+
+web.options("*", cors());
 
 web.get("/", (req, res) => {
     res.status(200).json({ status: "OK", message: "API is running" });
@@ -19,7 +22,3 @@ web.use(publicRouter);
 web.use(userRouter);
 
 web.use(errorMiddleware);
-
-// --- 8️⃣ Optional: global uncaught exception logging ---
-process.on("uncaughtException", (err) => console.error("Uncaught Exception:", err));
-process.on("unhandledRejection", (err) => console.error("Unhandled Rejection:", err));
