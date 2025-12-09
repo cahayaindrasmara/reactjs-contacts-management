fe5d52828ffe3ee302344aedc6d6d15c2411351d
cahayaindrasmara <indrasmara.cahaya27@gmail.com>
2025-12-09 15:15:51 +0700

    test

--- belajar-nodejs-restful-api/src/application/web.js
+++ belajar-nodejs-restful-api/src/application/web.js
@@ -5,20 +5,15 @@ import {userRouter} from "../route/api.js";
 import cors from "cors"
 
 export const web = express();
-web.use(cors({
-  origin: "https://reactjs-contacts-management.vercel.app",
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}))
+web.use(cors())
 
-web.options("*", cors());
+// web.options("*", cors());
 
 web.use(express.json());
 
-// web.get("/", (req, res) => {
-//     res.status(200).json({ status: "OK", message: "API is running" });
-// });
+web.get("/", (req, res) => {
+    res.status(200).json({ status: "OK", message: "API is running" });
+});
 
 web.use(publicRouter);
 web.use(userRouter);
